9c3616b7c43f3463997c4b1918575258f69db7fc
WangYuLei5433 <93036594+WangYuLei5433@users.noreply.github.com>
2021-11-02 12:49:20 +0900

    添加了新作业

---
+++ pythonコンピュータプログラミング概論/望月バドル课程作业/作业4/Unique.py
@@ -0,0 +1,6 @@
+l1=[1,1,2,2,3,3,4,5,6,6]
+l2=set()
+for i in range(len(l1)):
+    l2.add(l1[i])
+
+print(l2)
--- pythonコンピュータプログラミング概論/望月バドル课程作业/作业4/people.py
+++ pythonコンピュータプログラミング概論/望月バドル课程作业/作业4/people.py
@@ -1,2 +1,11 @@
-dc={'Alice':(22,'女性'),'Bob':(18,'男性'),'Diana':(19,'女性')}
-print(dc.keys)
+dc={'Alice':(22,'女性'),'Bob':(18,'男性'),'Charlie':(20,'男性'),'Diana':(19,'女性')}
+dcValues=list(dc.values())
+l1,l2=[],[]
+
+for a,b in dcValues:
+    if(a>=20):
+        l1.append((a,b))
+    if(b=='女性'):
+        l2.append((a,b))
+
+print('%s\n%s\n%s'%(dc,l2,l1))
